b06076f0fa2e55be8e3cc0b9c342df4add212dd2
XXX XXXXXX <michal.kurowski@edu.uekat.pl>
2026-05-20 23:37:41 +0200

    Login + register component with form validations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpErrorHandler } from './http-error-handler.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync(),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorHandler, multi: true },
   ],
 };
